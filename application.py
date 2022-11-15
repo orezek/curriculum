c4f454dda6b60487a2af41da74b7b5fe14badcb3
O.R <aldokezer@gmail.com>
2022-11-15 19:45:06 +0100

    update structure, design, add new sections

--- application.py
+++ application.py
@@ -1,12 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
 
 application = Flask(__name__)
+
+# variables
+email = "orezek@seznam.cz"
+be_number = "+320 497 127 267"
+cz_number = "+420 608 840 502"
+age = 38
+name = "Oldrich Rezek"
+greeting = "Hey, I'm"
+languages = ["English", "Czech"]
+
+skills = {"AWS Cloud": 80, "Windows Server": 82, "Networking": 75, "Python": 50, "HTML": 55, "Linux": 45,
+          "Problem Solving": 90, "VM Ware": 70, "Veeam": 60}
+
+interests = ["Technology",
+             "Blockchain",
+             "Finance"
+             "Sport",
+             "Photography",
+             "History",
+             "Philosophy",
+             "Travelling",
+             "Exploring"]
 
 
 @application.route("/")
 def index():
-    return "Hello World"
+    return render_template("resume.html", email=email, be_number=be_number,
+                           cz_number=cz_number, name=name, greeting=greeting, interests=interests)
 
 
 if __name__ == "__main__":
-    application.run(debug=True)
+    application.run(debug=True, port=5001)
