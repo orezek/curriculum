ae2625d21064bf1022ac9576cdaffcce52e6bab7
O.R <aldokezer@gmail.com>
2022-11-21 20:50:09 +0100

    data model update, hard coded strings changed in html templates for python variables

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from data_model import website_headers, interests, skills, profile_summary_data_prod, job_titles
 
 application = Flask(__name__)
 
@@ -11,24 +12,13 @@ name = "Oldrich Rezek"
 greeting = "Hey, I'm"
 languages = ["English", "Czech"]
 
-skills = {"AWS Cloud": 80, "Windows Server": 82, "Networking": 75, "Python": 50, "HTML": 55, "Linux": 45,
-          "Problem Solving": 90, "VM Ware": 70, "Veeam": 60}
-
-interests = ["Technology",
-             "Blockchain",
-             "Finance",
-             "Sport",
-             "Photography",
-             "History",
-             "Philosophy",
-             "Travelling",
-             "Exploring"]
-
 
 @application.route("/")
 def index():
     return render_template("resume.html", email=email, be_number=be_number,
-                           cz_number=cz_number, name=name, greeting=greeting, interests=interests)
+                           cz_number=cz_number, name=name, greeting=greeting,
+                           interests=interests, website_headers=website_headers,
+                           profile_summary_data_prod=profile_summary_data_prod, job_titles=job_titles)
 
 
 if __name__ == "__main__":
--- data_model.py
+++ data_model.py
@@ -1,20 +1,95 @@
 import json
 from pprint import pprint
 
-# data_model = {
-#     "principal": {{"principal_name": {"name": "data", "surname": "data"},
-#                    "principal_bio": {"dob": "data", "address": "data", "status": "data"},
-#                    "contacts": {"phone_number": ["data"], "email": ["data"],
-#                                 "social_nets": [{"social_net": "data"}]}}},
-#     "profile_info": {"profile_summary": "data"},
-#     "work_experience": [{"job_obj1": {"job_title": "data", "job_company": "data", "job_description": "data"}},
-#                         {"job_obj2": {"job_title": "data", "job_company": "data", "job_description": "data"}}],
-#     "skills": [{"skill1": "data"}, {"skill2": "data"}],
-#     "certifications": [{"cert1": "data"}, {"cert2": "data"}],
-#     "education": {"institution": "data"},
-#     "interests": ["data"]
-# }
+website_headers = {"contacts": "Contacts", "skills": "Skills", "certifications": "Certifications",
+                   "education": "Education", "interests": "Interests", "profile_summary": "Profile Summary",
+                   "work_experience": "Work Experience"}
 
+interests = ["Technology",
+             "Blockchain",
+             "Finance",
+             "Sport",
+             "Photography",
+             "History",
+             "Philosophy",
+             "Travelling",
+             "Exploring"]
+
+skills = {"AWS Cloud": 80, "Windows Server": 82, "Networking": 75, "Python": 50, "HTML": 55, "Linux": 45,
+          "Problem Solving": 90, "VM Ware": 70, "Veeam": 60}
+
+job_titles = ["System Administrator", "IT Manager", "Network Administrator"]
+
+profile_summary_data_prod = """I have been in the IT industry for more than a decade and I had spent time 
+in various roles. I started in a support role back in 2008 and progressively moved on to more technically advanced 
+roles and subsequently landing into more managerial position. During the years, I have acquired “industry standard” 
+skills as I was exposed to various products from the industry household names and infrastructure environments. VM 
+Ware, Veeam, Microsoft & Linux, Cisco, Aruba Ubiquity, Ruckus, Oracle and other software or hardware products to name 
+a few. However, as technology and approach to the IT is evolving so is my current interest. I have been lately 
+focused on cloud technologies and modern DevOps and SysOps approach to the IT discipline especially from the AWS 
+offerings. I am aware that the current trend in IT operations is moving fast into automation and knowing how to 
+handle a code is a necessity. Serverless, containers, cloud, CI/CD and git are not foreign buzzwords for me. 
+Nevertheless, during the last 5 years I was fortunate to had worked and lived in interesting places and experience 
+firsthand what others might read in adventure books. On the less bright side, I do not have much real-life exposure 
+into the DevOps and SysOps world as I was in environments that embraced the older way of “doing” IT. To keep my 
+career in progress and ensure that I have some minimal knowledge, I have recently passed, number of cloud 
+certifications and working on few more to solidify my skill set. Lately I have returned to the EU and I am looking 
+for an opportunity to work on an interesting project either in a full-time position or in a freelance role. The ideal 
+that I am most interested is a company that already have embraced or is in the process of moving into automation, 
+DevOps and SysOps operations and creates an interesting product."""
+
+profile_summary_data_v1 = """I am an experienced system and network administrator who is passionate about technology in 
+general. The last several years I have been living and working abroad. Either managing a whole IT department or 
+ensuring servers and networks are running smoothly and users/clients are happy. I am currently living in Brussels, 
+Belgium and planning to relocate back to Czech or Slovak republic during the springtime. My recent focus and aim is 
+to find an employment in cloud-oriented company where I can further develop my skills, knowledge and gain experience 
+in participating in related projects. Recently, I have been working on to validate my skills by taking several AWS 
+certifications exams to increase my chances to land on a cloud related job. My future employer should be or have 
+exposition into cloud technologies preferably in AWS cloud offerings. I am open to work both as a freelancer or as a 
+full-time employee."""
+
+profile_summary_data_v2 = """I have been in the IT industry for more than a decade and I had spent time in various 
+roles. I started in a support role back in 2008 and progressively moved on to more technically advanced roles and 
+subsequently landing into more managerial position. I am an experienced system and network administrator who is 
+passionate about technology in general. During the years, I have acquired “industry standard” skills as I was exposed 
+to various products from the industry household names and infrastructure environments. For example, VM Ware, Veeam, 
+Microsoft & Linux, Cisco, Aruba Ubiquity, Ruckus, Oracle and other software or hardware products to name a few. 
+However, as technology and approach to the IT in general is evolving so is my current interest. I have been lately 
+personally focused on cloud technologies and modern DevOps and SysOps approach to the discipline, especially from the 
+AWS offerings. I am aware that the current trends in IT operations are moving fast into the cloud, automation and as 
+the consequence for an individual in the industry, knowing how to handle a code in a repository or write a script is 
+a necessity. Serverless, containers, CI/CD and IaC is present and the future. In the last 5 years, I was fortunate to 
+had worked and lived in interesting places and experience firsthand what others might read in adventure books. On the 
+less bright side, I missed the real-life exposure into the DevOps and SysOps world as I was in environments that 
+embraced the older way of “doing” IT. To keep my career in touch with the current trends and ensure that I have some 
+minimal knowledge, I have recently passed number of cloud certifications and working on few more to solidify my skill 
+set. In June 2022, I returned back to the EU and I am looking for an opportunity to work on an interesting project 
+either in a full-time position or in a freelance role. The ideal that I am most interested in is a company that have 
+already embraced or is in the process of moving into the cloud, DevOps and SysOps world and creates an interesting 
+product. """
+
+soitron_job_description_data = """Maintaining and supporting IT infrastructure along with other office hardware and fleet of 
+client computers across several offices in Brussels Belgium. I am part of a two-man team that are hired to completely 
+outsource IT operations for a large international customer. We provide complete service from areas such as 
+consultancy, server and network management and extensive client support."""
+
+zuri_job_description_data = """Responsible for managing an IT department from managerial and technical perspective. 
+My main duty was to ensure 24/7 smooth IT operations for both internal and external customers with strong emphasis on 
+maintaining tight budget especially during the pandemic ara. I was leading a small team of local personnel with whom 
+we performed most of the tasks ourselves with excellent results. The place due to its nature needed 24/7 attention, 
+high speed and quality in execution was expected. The workload was very varied and rich, you were expected to handle 
+tasks from financial planning, hiring and attendance to negotiation with vendors, communication with government 
+agencies, sourcing and purchasing hardware to completely manage campus-like server, network and application 
+infrastructure. The size of the property was 14 acres and just to create a picture. There was around 220 active 
+access points over 11km of optical cabling, over 350 employees or over 100 high profile paying clients. I am very 
+happy to provide references and give you more details."""
+
+velaa_job_description_data = """Maintaining and supporting large network, server infrastructure, applications and 
+end-client devices on a secluded private island in the Maldives for top 1% net worth individuals. Very demanding 
+working environment with various tasks and projects usually with fast resolution times. References upon request. """
+
+olympus_job_description_data = """Responsible for maintaining Microsoft Sharepoint 2013 and 2016 server farm and 
+building business applications and workflows for internal clients. """
 
 user_data = {"user_info": {"user_full_name": {"name": "Oldrich", "surname": "Rezek"},
                            "user_bio": {"dob": "02051984", "age": "38", "nationality": "czech",
@@ -25,13 +100,34 @@ user_data = {"user_info": {"user_full_name": {"name": "Oldrich", "surname": "Rez
                                             "country": "belgium"},
                            "user_contacts": {"email": "orezek@seznam.cz", "phone_no": "+420608840502",
                                              "linkedin": "data", "github": "data"}},
-             "profile_info": {"profile_summary": "data"},
-             "work_experience": {"job_obj1": {"job_title": "data", "job_company": "data", "job_description": "data"}},
-             "skills": {"skill": "data", "skill1": "data"},
+             "profile_info": {"profile_summary": profile_summary_data_prod},
+             "work_experience": {"soitron": {"job_title": job_titles[0],
+                                             "job_company": "Soitron s.r.o - Brussels, Belgium - June 2022 - present",
+                                             "job_description": soitron_job_description_data},
+                                 "zuri": {"job_title": job_titles[1],
+                                          "job_company": "Zuri Zanzibar hotel and resort - Zanzibar - June 2019 - May "
+                                                         "2022", "job_description": zuri_job_description_data},
+                                 "velaa": {"job_title": job_titles[2],
+                                           "job_company": "Velaa Private Island - Maldives - October 2018 - May 2019",
+                                           "job_description": velaa_job_description_data},
+                                 "olympus": {"job_title": job_titles[0], "job_company": "Olympus sro - Prague Czech "
+                                                                                        "Republic - April 2016 - "
+                                                                                        "September 2018",
+                                             "job_description": olympus_job_description_data}},
+             "skills": {"AWS Cloud": 80, "Windows Server": 82, "Networking": 75, "Python": 50, "HTML": 55, "Linux": 45,
+                        "Problem Solving": 90, "VM Ware": 70, "Veeam": 60},
              "certification": {"cert1": "data", "cert2": "data"},
              "education": {"institution": "data"},
-             "interests": ["data"]
+             "interests": ["Technology",
+                           "Blockchain",
+                           "Finance",
+                           "Sport",
+                           "Photography",
+                           "History",
+                           "Philosophy",
+                           "Travelling",
+                           "Exploring"]
              }
 
-print(type(user_data))
-print(user_data["work_experience"]["job_obj1"])
+print(profile_summary_data_prod)
+print(type(profile_summary_data_prod))
