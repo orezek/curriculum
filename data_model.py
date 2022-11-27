61bd424b2196d6d0d8be9b07fec8bf91eded5a49
O.R <aldokezer@gmail.com>
2022-11-27 22:28:57 +0100

    Zuri job description edited

--- data_model.py
+++ data_model.py
@@ -107,13 +107,13 @@ consultancy, server and network management and extensive client support."""
 zuri_job_description_data = """Responsible for managing an IT department in luxurious hotel in Zanzibar, Tanzania. My 
 main duties were to ensure 24/7 smooth IT operations for both internal and external customers with strong emphasis on 
 maintaining tight budget especially during the pandemic ara. I was leading a small team of local personnel with whom 
-we performed most of the tasks ourselves with excellent results. The place due to its nature needed 24/7 attention, 
+we performed most of the tasks with excellent results. The place due to its nature needed 24/7 attention, 
 high speed and quality in execution was expected all the time. The workload was very varied and rich, 
 you were expected to handle tasks from financial planning, hiring and attendance to negotiation with vendors, 
 communication with government agencies, sourcing and purchasing hardware to completely manage campus-like server, 
-network and application infrastructure. The size of the property was 14 acres there was around 220 active access 
-points over 11km of optical cabling, over 350 employees or over 100 high profile paying clients just to create a 
-picture. I am very happy to provide references and give you more details. """
+network and application infrastructure. The size of the property was 14 acres, there was around 220 active access 
+points, over 11km of optical cabling, over 350 employees or over 100 high profile paying clients just to create a 
+picture. I am very happy to provide references upon request. """
 
 velaa_job_description_data = """Maintaining and supporting large network, server infrastructure, applications and 
 end-client devices on a secluded private island in the Maldives for top 1% net worth individuals. Very demanding 
